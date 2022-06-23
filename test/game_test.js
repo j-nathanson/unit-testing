a90203d52b87caa0df44f3d5c369500c5b5606c6
jacobnathanson <jacobnath93@gmail.com>
2022-06-23 12:54:28 -0400

    pending tests

---
+++ test/game_test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const { describe } = require("mocha");
+
+xdescribe('GAME INSTANCE FUNCTIONS', function () {
+    describe('checkGameStatus', function () {
+        // pending, do not add a function
+        it('should tell me when the game is over')
+    })
+
+    // if you do add a function you can place x before it or desribe 
+    it('should tell me when the game is over', function(){})
+})
